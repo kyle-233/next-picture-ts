06f53460642c244d2e81441fd9ce7fd4fbff1489
kyle-233 <lubingfu@chinabzc.com>
2024-05-20 17:53:26 +0800

    feat(background): complete star background

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { StarBackground } from './_components'
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <div className={styles.background}>
+        <StarBackground />
+      </div>
     </main>
   );
 }
